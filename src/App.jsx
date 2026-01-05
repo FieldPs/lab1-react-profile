13a783ed0033d4155db2833a821b26390eced25e
garfield <pcr.pol101@gmail.com>
2026-01-05 14:52:05 +0700

    feat: implement add skill tags

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ProfileCard from "./components/ProfileCard"
 
 function App() {
   const [githubData, setGithubData] = useState(null);
-  const username = "FieldPS"
+  const username = "FieldPS";
 
   useEffect(() => {
     fetch(`https://api.github.com/users/${username}`)
@@ -13,6 +13,15 @@ function App() {
     })
     .catch(err => console.error(err));
   }, []);
+
+  const [skills, setSkills] = useState(['React', 'Git']);
+  const [newSkill, setNewSkill] = useState("");
+  const addSkill = () => {
+      if (newSkill.trim() !== "") {
+          setSkills([...skills, newSkill.trim()]);
+          setNewSkill("");
+      }
+  };
   
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
@@ -27,6 +36,21 @@ function App() {
       ) : (
         <p>Loading data from Github...</p>
       )}
+      <div>
+        <input
+            type="text"
+            value={newSkill}
+            onChange={(e) => setNewSkill(e.target.value)}
+            placeholder="Add a new skill"
+        />
+        <button onClick={addSkill}>Add Skill</button>
+        <h3>Skills:</h3>
+        <ul>
+            {skills.map((skill, index) => (
+                <li key={index}>{skill}</li>
+            ))}
+        </ul>
+      </div>
     </div>
   )
 }
